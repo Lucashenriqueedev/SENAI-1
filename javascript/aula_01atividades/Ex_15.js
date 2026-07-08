adb07d7b29c68dc3c4f723e99e0a93fc4f308bcf
LUCAS HENRIQUE DA SILVA SOUSA VIEIRA <lucas_hs_vieira@estudante.sesisenai.org.br>
2026-07-08 14:03:28 -0300

    teste

---
+++ javascript/aula_01atividades/Ex_15.js
@@ -0,0 +1,10 @@
+// === Lucas 08/07/26 === //
+// === Esse script mostra um pouco da minha vida === //
+// === Informações === //
+let nome = "Lucas"
+let idade = 15
+let hobby = "jogar basquete e farmar aura"
+const cidadentl = "São José"
+const profissaodosh = "jogador de basquete ou programador trabalhando pra fora"
+// === Exibição === //
+console.log(`Prazer meu nome é ${nome} e tenho ${idade} meu hobby é ${hobby} e minha cidade natal é ${cidadentl} e no futuro quero ser ${profissaodosh}`)
